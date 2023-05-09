f560b37dc1a1e58fba03b95ba77992c5b6bb6ed7
Jason Li <jasonli99193@gmail.com>
2023-05-09 15:22:16 -0500

    Jason data

---
+++ scripts/database.js
@@ -0,0 +1,104 @@
+
+
+
+const database = {
+    AreaService: [{
+        id: 1,
+        AreaId: ,
+        ServiceId: ,
+
+    }, {
+        id: 2,
+        AreaId: ,
+        ServiceId: ,
+
+    }, {
+        id: 3,
+        AreaId: ,
+        ServiceId: ,
+
+    }, {
+        id: 4,
+        AreaId: ,
+        ServiceId: ,
+
+    }, {
+        id: 5,
+        AreaId: ,
+        ServiceId: ,
+
+    }, {
+        id: 6,
+        AreaId: ,
+        ServiceId: ,
+
+    }, {
+        id: 7,
+        AreaId: ,
+        ServiceId: ,
+
+    }, {
+        id: 8,
+        AreaId: ,
+        ServiceId: ,
+
+    }, {
+        id: 9,
+        AreaId: ,
+        ServiceId: ,
+
+    }, {
+        id: 10,
+        AreaId: ,
+        ServiceId: ,
+    }],
+    GuestService: [{
+        id: 1,
+        GuestId: ,
+        ServiceId: ,
+    },{
+        id: 2,
+        GuestId: ,
+        ServiceId: ,
+    },{
+        id: 3,
+        GuestId: ,
+        ServiceId: ,
+    },{
+        id: 4,
+        GuestId: ,
+        ServiceId: ,
+    },{
+        id: 5,
+        GuestId: ,
+        ServiceId: ,
+    },{
+        id: 6,
+        GuestId: ,
+        ServiceId: ,
+    },{
+        id: 7,
+        GuestId: ,
+        ServiceId: ,
+    },{
+        id: 8,
+        GuestId: ,
+        ServiceId: ,
+    },{
+        id: 9,
+        GuestId: ,
+        ServiceId: ,
+    },{
+        id: 10,
+        GuestId: ,
+        ServiceId: ,
+    }],
+
+
+
+
+
+
+
+
+}
